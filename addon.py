6ea367c86115df72360184d377743f62c72a8668
Mehmed Mert <mehmed.mert@quartett-mobile.de>
2017-02-12 18:24:10 +0100

    Small fix

--- addon.py
+++ addon.py
@@ -18,4 +18,5 @@ xmbcplugin.setContent(addon_handle, 'tv')
 
 url = 'blob:http://www.trt.net.tr/e5daf217-13db-044c-9caf-3692b33e4336'
 li = xbmcgui.ListItem('TRT 1 HD', 'DefaultVidao.png')
+xmbcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 xmbcplugin.endOfDirectory(addon_handle)
