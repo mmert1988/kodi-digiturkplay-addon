5653f4293ed1ed21c0ad6ddb3807b28921635f18
Mehmed Mert <mehmed.mert@quartett-mobile.de>
2017-02-12 18:12:14 +0100

    Testing TRT 1 HD

--- addon.py
+++ addon.py
@@ -1,11 +1,21 @@
+import sys
 import xbmcaddon
 import xbmcgui
+import xmbcplugin
  
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
-line1 = "Hello World!"
-line2 = "We can write anything we want here"
-line3 = "Using Python"
+#line1 = "Hello World!"
+#line2 = "We can write anything we want here"
+#line3 = "Using Python"
  
-xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+#xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+
+addon_handle = int(sys.argv[1])
+
+xmbcplugin.setContent(addon_handle, 'tv')
+
+url = 'blob:http://www.trt.net.tr/e5daf217-13db-044c-9caf-3692b33e4336'
+li = xbmcgui.ListItem('TRT 1 HD', 'DefaultVidao.png')
+xmbcplugin.endOfDirectory(addon_handle)
